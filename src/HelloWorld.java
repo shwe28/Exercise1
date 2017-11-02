959307eb57155c7264c102d6f347b83fac32b6ef
DESKTOP-3O8PR0H\Shwetha <suresh.shwetha@gmail.com>
2017-11-02 21:46:50 +0100

    Commit HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello ASE2017 - How are you? :)");
+		System.out.println("Hello ASE2017 - How are you now? :)");
+		System.out.print("Please enter your name : ");
+		Scanner sc = new Scanner(System.in);
+		String userName = sc.nextLine();
+		HelloUser user = new HelloUser();
+		user.greetUser(userName);
+		sc.close();
 	}
 
 }
